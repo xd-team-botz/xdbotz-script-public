29491d564ff4e2ad8e9ae0902831a2948d5e2ef3
aaabiiil <81213131+aaabiiil@users.noreply.github.com>
2023-03-27 01:24:23 +0700

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 const chalk = require('chalk')
 // EDIT DISINI
-global.owner = ['6282127075205'] // no own
-global.author = 'KurrXd' // nama author 
-global.packname = 'xd bot whatsapp' // nama pack sticker
-global.namabot = 'XD BOTZ' // nama bot mu
-global.group = 'https://bit.ly/xdbotwhatsapp' // grup mu
+global.owner = ['6288973078495'] // no own
+global.author = 'Abil De Fuge' // nama author 
+global.packname = 'babu abil whatsapp' // nama pack sticker
+global.namabot = 'SHIRO BOT' // nama bot mu
+global.group = 'https://chat.whatsapp.com/FcVEQtWtLOG5XHMFCIbP1c' // grup mu
 global.pic = 'https://telegra.ph/file/36211850fa46193ef96ad.jpg' // logo lu
 
 
